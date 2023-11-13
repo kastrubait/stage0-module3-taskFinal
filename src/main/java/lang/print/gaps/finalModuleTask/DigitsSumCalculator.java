5f22a836585926297aa389f711349f34d7528168
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-13 12:29:37 +0300

    feat: complete final tast module3 v.0

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,23 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = 0;
+        int numberChange = number;
+
+        int digit = numberChange % 10;
+        sum += digit;
+        numberChange /= 10;
+
+        digit = numberChange % 10;
+        sum += digit;
+        numberChange /= 10;
+
+        digit = numberChange % 10;
+        sum += digit;
+        numberChange /= 10;
+
+        sum += numberChange;
+
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int last = number % 10;
+        int prev = number / 10;
+        int medium = prev % 10;
+        int first = prev / 10;
+        System.out.println(last + medium + first);
     }
 }
